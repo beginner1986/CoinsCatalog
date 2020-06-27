b6dc032be8df4a17bc23148402ec5220134534f5
Adam Emieljaniuk <beginner1986@gmail.com>
2020-06-27 11:47:17 +0200

    Home page template and Coin class added.

---
+++ CoinsCatalog/src/main/java/pl/beginner/model/Coin.java
@@ -0,0 +1,23 @@
+package pl.beginner.model;
+
+import lombok.Data;
+
+@Data
+public class Coin {
+
+	private long id;
+	private String name;
+	private String code;
+	private String year;
+	private String diameter;
+	private String shape;
+	private String inscription;
+	private String circulation;
+	private String metal;
+	private String fineness;
+	private String weight;
+	private String stamp;
+	private String border;
+	private String comment;
+	private String photo;
+}
