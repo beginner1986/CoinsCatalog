ea6d2974fc45b9e47dbdfaca459dd0cbc0697d64
Adam Emieljaniuk <beginner1986@gmail.com>
2020-06-27 11:30:03 +0200

    Initial commit.

---
+++ CoinsCatalog/src/main/java/pl/beginner/CoinsCatalogApplication.java
@@ -0,0 +1,13 @@
+package pl.beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinsCatalogApplication.class, args);
+	}
+
+}
---
+++ CoinsCatalog/src/test/java/pl/beginner/CoinsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
